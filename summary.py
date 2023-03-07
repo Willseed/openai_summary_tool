9642c4dc8cac271a8a98f0fe84f9e7e81fcad62f
pony <ponylai@tdcc.com.tw>
2023-03-07 12:01:01 +0800

    Optimized file reading for improved performance.

--- summary.py
+++ summary.py
@@ -1,3 +1,4 @@
+from typing import Generator
 import openai
 
 class Summary:
@@ -6,22 +7,27 @@ class Summary:
         openai.api_key = token
         self.text_filename = text_filename
         self.summary_filename = summary_filename
-    
+
+    def __read_file(self) -> Generator[str, None, None]:
+        with open(self.text_filename, 'r', encoding='utf-8') as file:
+            # 讀取檔案內容，並去除換行符號
+            content = file.read().replace('\n', '')
+            # 切割字串，每個元素字數不大於 1000
+            for i in range(0, len(content), 1000):
+                yield content[i:i+1000]
+
     def get_summary(self) -> None:
         summary_list = []
         with open(self.text_filename, 'r', encoding='utf-8') as f:
-            for line in f.readlines():
-                paragraph_len = len(line) // 1000 + 1
-                for j in range(paragraph_len):
-                    paragraph = line[j * 1000 : (j + 1) * 1000]
-                    completion = openai.ChatCompletion.create(
-                        model = "gpt-3.5-turbo",
-                        messages = [
-                                {"role": "system", "content": "請你成為文章摘要的小幫手，摘要以下文字，以繁體中文輸出"},
-                                {"role": "user", "content": paragraph}
-                            ]
-                    )
-                    summary_list.append(completion.choices[0].message.content)
+            for paragraph in self.__read_file():
+                completion = openai.ChatCompletion.create(
+                    model = "gpt-3.5-turbo",
+                    messages = [
+                            {"role": "system", "content": "請你成為文章摘要的小幫手，摘要以下文字，以繁體中文輸出"},
+                            {"role": "user", "content": paragraph}
+                        ]
+                )
+                summary_list.append(completion.choices[0].message.content)
         with open(self.summary_filename, 'w', encoding='utf-8') as f:
             for i in summary_list:
                 f.write(i + '\n')
